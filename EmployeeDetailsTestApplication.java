e0045f0cd7913dd5cab0f392e37eea94f3e58d40
Rutuja Pardeshi <65347296+Rutujasnp@users.noreply.github.com>
2023-06-07 04:53:46 -0700

    Add files via upload

---
+++ EmployeeDetailsTestApplication.java
@@ -0,0 +1,13 @@
+package com.SystemTest.EmployeeDetailsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDetailsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDetailsTestApplication.class, args);
+	}
+
+}
---
+++ EmployeeDetailsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SystemTest.EmployeeDetailsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDetailsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
